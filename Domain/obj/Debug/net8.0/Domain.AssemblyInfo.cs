23969689e55dd09f82ddb9de10704e8d58f0536e
Christian Politis <christianpolitis@gmail.com>
2024-08-26 09:08:09 +1000

    Section 4 complete

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34dadf85e63cc18854002e6a4df7306271c78de0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d0479a77081ff7fcaca6e73e942b1a710946a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
