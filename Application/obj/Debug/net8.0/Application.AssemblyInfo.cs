204359d745da47adb6803d9a6d0fd83fbd43a23c
Christian Politis <christianpolitis@gmail.com>
2024-09-11 17:15:53 +1000

    Section 9 Complete

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a889c90c66cc0d2860f3af56b11de1f41add75c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f866df63f3093826eda39455396ed96ffebd41")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
 import './app/layout/styles.css'
 import {store, StoreContext} from "./app/stores/store.ts";
 import {RouterProvider} from "react-router-dom";
-import {router} from "./app/router/routes.tsx";
+import {router} from "./app/router/Routes.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
