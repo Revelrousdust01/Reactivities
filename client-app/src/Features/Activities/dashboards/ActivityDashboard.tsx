e9f866df63f3093826eda39455396ed96ffebd41
Christian Politis <christianpolitis@gmail.com>
2024-09-11 13:51:34 +1000

    Finished Section 8

--- client-app/src/Features/Activities/dashboards/ActivityDashboard.tsx
+++ client-app/src/Features/Activities/dashboards/ActivityDashboard.tsx
@@ -1,26 +1,29 @@
 import {Grid} from "semantic-ui-react";
 import ActivityList from "./ActivityList.tsx";
-import ActivityDetails from '../details/ActivityDetails.tsx';
-import ActivityForm from "../form/ActivityForm.tsx";
 import {useStore} from "../../../app/stores/store.ts";
 import {observer} from "mobx-react-lite";
+import {useEffect} from "react";
+import LoadingComponent from "../../../app/layout/LoadingComponent.tsx";
 
 export default observer(function ActivityDashboard() {
-    
-    const{activityStore} = useStore();
-    const{selectedActivity, editMode} = activityStore;
+
+    const {activityStore} = useStore();
+    const {loadActivities, activityRegistry} = activityStore;
+
+    useEffect(() => {
+        if(activityRegistry.size <= 0) loadActivities();
+    }, [loadActivities, activityRegistry.size]);
+
+    if(activityStore.loadingInitial)
+        return <LoadingComponent content='Loading app' />
+
     return (
         <Grid>
             <Grid.Column width={'10'}>
             <ActivityList />
             </Grid.Column>
             <Grid.Column width={'6'}>
-                {selectedActivity && !editMode &&
-                <ActivityDetails />}
-                {
-                    editMode &&
-                    <ActivityForm />
-                }
+                <h2>Activity Filters</h2>
             </Grid.Column>
         </Grid>
     )
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,22 @@
-import {useEffect} from "react";
 import {Container} from "semantic-ui-react";
 import NavBar from "./NavBar.tsx";
-import ActivityDashboard from "../../Features/Activities/dashboards/ActivityDashboard.tsx";
-import LoadingComponent from "./LoadingComponent.tsx";
-import {useStore} from "../stores/store.ts";
 import {observer} from "mobx-react-lite";
+import {Outlet, useLocation} from "react-router-dom";
+import HomePage from "../../Features/home/HomePage.tsx";
 
 function App() {
-    const {activityStore} = useStore();
-  
-  useEffect(() => {
-    activityStore.loadActivities();
-  }, [activityStore]);
-  
-  if(activityStore.loadingInitial)
-      return <LoadingComponent content='Loading app' />
-  
+  const location = useLocation();
+
   return (
       <>
-          <NavBar />
-          <Container style={{marginTop: '7em'}}>
-            <ActivityDashboard />
-          </Container>
+          {location.pathname === '/' ? <HomePage/> : (
+            <>
+              <NavBar />
+              <Container style={{marginTop: '7em'}}>
+              <Outlet />
+              </Container>
+            </>
+          )}
       </>
   )
 }
