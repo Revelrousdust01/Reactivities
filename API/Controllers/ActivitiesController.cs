b4a77b3cbb7f4ffdcfce3f1f84b664ab1f01ddbc
Christian Politis <christianpolitis@gmail.com>
2024-09-13 11:17:48 +1000

    Completed Section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -8,36 +8,30 @@ public class ActivitiesController : BaseApiController
 {
 
     [HttpGet] //api/activities
-    public async Task<ActionResult<List<Activity>>> GetActivities()
-        => await Mediator.Send(new List.Query());
+    public async Task<IActionResult> GetActivities()
+        => HandleResult(await Mediator.Send(new List.Query()));
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        => await Mediator.Send(new Details.Query{Id = id});
+    public async Task<IActionResult> GetActivity(Guid id)
+    {
+        var result = await Mediator.Send(new Details.Query { Id = id });
+
+        return HandleResult(result);
+    }
 
     [HttpPost]
     public async Task<IActionResult> CreateActivity(Activity activity)
-    {
-        await Mediator.Send(new Create.Command { Activity = activity });
-        
-        return Ok();
-    }
+        => HandleResult(await Mediator.Send(new Create.Command { Activity = activity }));
 
     [HttpPut("{id}")]
     public async Task<IActionResult> EditActivity(Guid id, Activity activity)
     {
         activity.Id = id;
-        await Mediator.Send(new Edit.Command { Activity = activity });
-        
-        return Ok();
+        return HandleResult(await Mediator.Send(new Edit.Command { Activity = activity }));
     }
 
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteActivity(Guid id)
-    {
-        await Mediator.Send(new Delete.Command { Id = id });
-
-        return Ok();
-    }
+        => HandleResult(await Mediator.Send(new Delete.Command { Id = id }));
     
 }
---
+++ client-app/src/Features/errors/ServerError.tsx
@@ -0,0 +1,18 @@
+﻿import {useStore} from "../../app/stores/store.ts";
+import {Container, Header, Segment} from "semantic-ui-react";
+
+export default function ServerError(){
+    const {commonStore} = useStore();
+    return (
+        <Container>
+            <Header as={'h1'} content={'Server Error'} />
+            <Header sub as={'h5'} color={'red'} content={commonStore.error?.message} />
+            {commonStore.error?.details && (
+                <Segment>
+                    <Header as={'h4'} content={'Stack trace'} color={"teal"} />
+                    <code style={{marginTop: '10px'}}>{commonStore.error.details}</code>
+                </Segment>
+            )}
+        </Container>
+    )
+}
