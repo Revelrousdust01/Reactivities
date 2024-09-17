8f402464637f2541d9f1598620d00d709a19075a
Christian Politis <christianpolitis@gmail.com>
2024-09-17 13:49:12 +1000

    Migrations

---
+++ Persistence/Migrations/20240917004840_ChangePhotoBioToURL.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePhotoBioToURL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Bio",
+                table: "Photos",
+                newName: "Url");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Url",
+                table: "Photos",
+                newName: "Bio");
+        }
+    }
+}
