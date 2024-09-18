e4401691635809261f6e46b7af50f243eb563151
Christian Politis <christianpolitis@gmail.com>
2024-09-18 15:12:42 +1000

    Section 18 Finished

--- API/Controllers/ProfilesController.cs
+++ API/Controllers/ProfilesController.cs
@@ -8,4 +8,8 @@ public class ProfilesController : BaseApiController
     [HttpGet("{username}")]
     public async Task<IActionResult> GetProfile(string username)
         => HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+    
+    [HttpPut]
+    public async Task<IActionResult> Edit(Edit.Command command)
+        => HandleResult(await Mediator.Send(command));
 }
--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f402464637f2541d9f1598620d00d709a19075a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06debf14b92c351f8e9432d79098b558047dc8f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
